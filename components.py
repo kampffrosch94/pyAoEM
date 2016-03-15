25dbf08f884f5968995ea9d979c1e9555042ae9a
kampffrosch94 <johanneskarl.dev@gmail.com>
2016-03-15 02:05:29 +0100

    cleanup components.py

--- components.py
+++ components.py
@@ -1,12 +1,12 @@
-from sdl2 import *
-from utility import Position
-from errors import SDL_Exception
-from ctypes import byref
+import sdl2
+import ctypes
+import utility
+import errors
 __doc__ = """This File holds the various components of the ecs.
 
 A component should only hold data and no functionality if possible."""
 
-class MapPos(Position):
+class MapPos(utility.Position):
     """The position of an entity on the map."""
 
 class Buffer(object):
@@ -21,7 +21,7 @@ class StartBuffer(Buffer):
 
 class Graphic(object):
     """Contains a texture and the position where it should be rendered.
-    
+
     Change x and y to change the position where the graphic will be
     rendered from the RenderSystem.
     """
@@ -33,21 +33,24 @@ class Graphic(object):
         self.x = x
         self.y = y
         if w == 0 or h == 0:
-            flags = Uint32()
-            access = c_int()
-            w = c_int()
-            h = c_int()
-            ret = SDL_QueryTexture(texture, byref(flags), 
-                        byref(access), byref(w), byref(h))
+            flags = sdl2.Uint32()
+            access = sdl2.c_int()
+            w = sdl2.c_int()
+            h = sdl2.c_int()
+            ret = sdl2.SDL_QueryTexture(texture,
+                                        ctypes.byref(flags),
+                                        ctypes.byref(access),
+                                        ctypes.byref(w),
+                                        ctypes.byref(h))
             if ret == -1:
-                raise SDL_Exception()
+                raise errors.SDL_Exception()
             w = w.value
             h = h.value
 
         self.w = w
         self.h = h
-        self.src_rect = SDL_Rect(0,0,w,h)
-        self._dest_rect = SDL_Rect(x,y,w,h)
+        self.src_rect = sdl2.SDL_Rect(0,0,w,h)
+        self._dest_rect = sdl2.SDL_Rect(x,y,w,h)
         self.active = True
 
     @property
