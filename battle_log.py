0a6f88c072dd6fe4992d2106e2339a8c874cd5db
kampffrosch94 <johanneskarl.dev@gmail.com>
2016-03-26 02:22:38 +0100

    update battle_log with new rendermethods

--- battle_log.py
+++ battle_log.py
@@ -1,16 +1,9 @@
 """Holds and Renders the Messagelog in battles."""
-import sdl2
 import res
 
 messages = []
 
-log_src = sdl2.SDL_Rect(w=640, h=160)
-log_dest = sdl2.SDL_Rect(x=0, y=480, w=log_src.w, h=log_src.h)
-
-texture = sdl2.SDL_CreateTexture(
-    res.renderer,
-    sdl2.SDL_PIXELFORMAT_RGBA8888,
-    sdl2.SDL_TEXTUREACCESS_TARGET,log_src.w,log_src.h)
+graphic = res.create_graphic(x=0, y=480, w=640, h=160)
 
 dirty = False
 
@@ -24,22 +17,18 @@ def add_msg(msg):
 def update():
     global dirty
     if dirty:
-        sdl2.SDL_SetRenderTarget(res.renderer,texture)
-        sdl2.SDL_RenderClear(res.renderer)
+        graphic.make_render_target()
+        res.render_clear()
         y = 0
         for msg in messages:
             g = res.create_text_graphic(msg)
             g.y = y
-            sdl2.SDL_RenderCopy(
-                res.renderer,
-                g.texture,
-                g.src_rect,
-                g.dest_rect)
+            g.render()
             y += g.h
-            sdl2.SDL_DestroyTexture(g.texture)
-        sdl2.SDL_SetRenderTarget(res.renderer,None)
+            g.destroy()
+        res.reset_render_target()
         dirty = False
 
 def render():
     update()
-    sdl2.SDL_RenderCopy(res.renderer,texture,log_src,log_dest)
+    graphic.render()
