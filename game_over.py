211b01e8842f69e739c27728bc8f27e792a9c757
kampffrosch94 <johanneskarl.dev@gmail.com>
2016-08-06 19:52:35 +0200

    tmpfix for hangup after win through freeaction

--- game_over.py
+++ game_over.py
@@ -1,20 +1,26 @@
 import sdl2
 import res
 import input_
+import battle
 
 title = None
 choice = res.create_text_graphic(
     "a) Alright. (Quit.)",
     x=100, y=200)
 
+
 def quit_():
+    # TODO proper fix for hangup after not turnending action kills last foe
+    battle.bind_keys()  # this makes it possible to spent last action
     input_.quit_handler()
+
 
 def render():
     res.render_clear()
     title.render()
     choice.render()
     res.render_present()
+
 
 def activate(won):
     global title
